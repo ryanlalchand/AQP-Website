6444045c146d0c85f263298840dba97222dcf2ab
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2021-11-28 20:31:11 -0500

    added example credentials so mine aren't public

---
+++ credentials/example_credentials.js
@@ -0,0 +1,8 @@
+//rename this file to credentials.js and fill in your credentials
+const credentials = {
+    host : 'localhost',
+    user : 'root',
+    password : 'password'
+  };
+  
+module.exports = {credentials};
