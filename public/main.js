6f9d07fd8a27f8ab608c882111629205bcd34731
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-02-09 15:30:56 -0500

    fixed mysql errors!!!!

--- public/main.js
+++ public/main.js
@@ -1,31 +1,22 @@
-async function fetchData() {
-  async function processForm(e) {
-    if (e.preventDefault) e.preventDefault();
-    console.log(e);
-    try {
-      const response = await fetch("/DBrequest", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: document.getElementById("query").value,
-        }),
-      });
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-
-    // You must return false to prevent the default form behavior
-    return false;
+async function processForm(e) {
+  if (e.preventDefault) e.preventDefault();
+  console.log(e);
+  try {
+    const response = await fetch("/DBrequest", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: document.getElementById("query").value,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
   }
 
-  var form = document.getElementById("form");
-  if (form.attachEvent) {
-    form.attachEvent("submit", processForm);
-  } else {
-    form.addEventListener("submit", processForm);
-  }
+  // You must return false to prevent the default form behavior
+  return false;
 }
