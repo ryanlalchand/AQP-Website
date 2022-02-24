093d55c2c713132679ff63211af08e8ac7e00585
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-02-23 19:00:30 -0500

    attempting to add easytimer.js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
+import { easytimer } from "./easytimer.min.js";
+let timer = easytimer.Timer();
+
 async function processForm(e) {
   if (e.preventDefault) e.preventDefault();
   console.log(e);
   try {
+    timer.start();
+
     const response = await fetch("/DBrequest", {
       method: "POST",
       headers: {
@@ -12,10 +17,16 @@ async function processForm(e) {
         size: document.getElementById("size").value,
       }),
     });
+
     const data = await response.json();
     console.log(JSON.stringify(data));
-
     document.getElementById("OGanswer").innerHTML = JSON.stringify(data);
+
+    timer.stop();
+    console.log(timer.getTimeValues().toString());
+    document.getElementById("OGtime").innerHTML = timer
+      .getTimeValues()
+      .toString();
   } catch (error) {
     console.log(error);
   }
